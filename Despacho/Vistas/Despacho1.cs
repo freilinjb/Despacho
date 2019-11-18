17d62bd5ae932c23d58c1b46ee8c18e7cf2a6285
Freilin Jose <freilinjb@gmail.com>
2019-11-18 18:20:16 -0400

    Eventos de obra en funcionando

---
+++ Despacho/Vistas/Despacho1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace Despacho.Vistas
+{
+    public partial class Despacho1 : Telerik.WinControls.UI.RadForm
+    {
+        public Despacho1()
+        {
+            InitializeComponent();
+        }
+    }
+}
